6c4fbd76373af377547b044589d7d5055c475d48
Brian Windson <60999180+brianwindson@users.noreply.github.com>
2022-11-23 17:38:47 -0300

    n lembrokkk

---
+++ src/main/java/br/edu/ifsp/spo/projeto/utils/Regions.java
@@ -0,0 +1,11 @@
+package br.edu.ifsp.spo.projeto.utils;
+
+public class Regions {
+	private String state;
+	
+	public Regions(String state) {
+		if (state ==  'goiás' or 'amazonas', 'pará', 'mato grosso', "mato grosso do sul",
+				"distrito federal");
+	}
+
+}
